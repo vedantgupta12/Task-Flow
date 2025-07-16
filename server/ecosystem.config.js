132e4f4951b0bc2d5d70e784127d59837c2181c3
vedantgupta12 <vedantgupta370@gmail.com>
2025-07-16 10:40:22 +0530

    server modification

---
+++ server/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports={
+    apps:[
+        {
+         name:"Task Flow",
+         script:"npm",
+         args:"run dev",
+         env:{
+            NODE_ENV:"development",
+         },
+
+        },
+    ]
+}
