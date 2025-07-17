57f78e542e3538d6c14e6435040bf0f9881ae1e3
vedantgupta12 <vedantgupta370@gmail.com>
2025-07-17 17:41:47 +0530

    redirect images to s3

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tf-s3-images.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -59,7 +59,7 @@ const Sidebar = () => {
         </div>
         {/*Team*/}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
-          <Image src="/logo.png" alt="Logo" width={40} height={40} />
+          <Image src="https://tf-s3-images.s3.us-east-1.amazonaws.com/logo.png" alt="Logo" width={40} height={40} />
           <div>
             <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
               TEAM{" "}
